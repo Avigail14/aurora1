bd4383919aed93f2e0d8da7917afe2b57b9f464f
Bear2137 <evlingovzman@gmail.com>
2024-02-29 19:53:02 +0200

    add start of room3

---
+++ AURORA/aurora/js/room3_aurora.js
@@ -0,0 +1,7 @@
+function hint_room3(){
+    window.alert("I have to find my books! help me search the library!");
+}
+
+function books(){
+    window.alert("Those are my old books! lets have a look together, maybe well find something ;)");
+}
