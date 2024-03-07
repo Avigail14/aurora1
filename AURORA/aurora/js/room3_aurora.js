4071449fe49764eed1100b2f495b7d8cd816602a
Bear2137 <evlingovzman@gmail.com>
2024-03-07 19:51:57 +0200

    add room3 functions

--- AURORA/aurora/js/room3_aurora.js
+++ AURORA/aurora/js/room3_aurora.js
@@ -3,5 +3,12 @@ function hint_room3(){
 }
 
 function books(){
-    window.alert("Those are my old books! lets have a look together, maybe well find something ;)");
+    window.alert("Those are my old books! I loved them but now therir'e usless, if I tak them tell slow me down.)");
 }
+function poision(){}
+
+showWitch();{
+    document.getElementById("witch").style.display = "block";
+    
+}
+setTimeout(showWitch(),120000);
