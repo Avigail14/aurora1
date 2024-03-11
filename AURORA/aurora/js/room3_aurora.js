19508f41895d8e3ec30e8fa62f064767f1309874
Bear2137 <evlingovzman@gmail.com>
2024-03-11 19:58:04 +0200

    add room3 functions, fix alittle pictures

--- AURORA/aurora/js/room3_aurora.js
+++ AURORA/aurora/js/room3_aurora.js
@@ -1,14 +1,28 @@
 function hint_room3(){
-    window.alert("I have to find my books! help me search the library!");
+    window.alert("I have to find something that can help me out!");
 }
-
+function oldBook(){
+    window.alert("just an old book. lets keep looking");
+}
 function books(){
-    window.alert("Those are my old books! I loved them but now therir'e usless, if I tak them tell slow me down.)");
+    window.alert("Those are my old books! I loved them but now they are usless, if I take them tell slow me down.)");
 }
-function poision(){}
-
-showWitch();{
+function poision(){
+   window.alert("Wait, this poision must have ben spilld from somewhere. Oh there is a leather bag! lets check it out");
+   document.getElementById("leatherBag").style.display="block";
+}
+s
+function showWitch(){
     document.getElementById("witch").style.display = "block";
     
+}
+function leather(){
+    window.alert("There it is, lets have a look");
+    document.getElementById("note").style.display = "block";
+}
+
+function showNextRoomButton(){
+    document.getElementById("nextRoom4").style.display = "block";
+
 }
 setTimeout(showWitch(),120000);
